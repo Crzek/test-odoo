c01b789cc484feba86f506aae064c570b8e6201d
CruzWin <javier1996.ec@gmail.com>
2023-08-29 16:03:42 +0200

    contact

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Test Odoo Cajas - Erick Cruz',
+    'version': '1.0',
+    'summary': 'control completo cajas',
+    'description': 'Control y gestion de cajas de la empresa',
+    'author': 'Erick Cruz Cedeño',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'summary': 'Short summary.',
+    'depeds': ['base', 'contacts', 'stock', 'product'],
+    'data': [
+        'views/contacts/view_age.xml',
+        # 'views/stock/view_inh.xml',
+        # 'security/security.xml'
+        # 'security/ir.model.access.csv'
+    ],
+    'images': ['./static/description/icon.png']
+}
---
+++ models/contact_irh.py
@@ -0,0 +1,56 @@
+from odoo import models, fields
+
+
+class Contact_irh(models.Model):
+    # hereda de contacto y productos (stock)
+    _inherit = ['res.partner']
+
+    # campos
+    age = fields.Integer(string="Edad")
+    products = fields.One2many(
+        comodel_name='product.product',  # tabla a la que se relaciona
+        inverse_name='contact_id',  # campo de la tabla relacionada
+        string="Productos"  # nombre del campo
+    )
+
+
+class Stock_irh(models.Model):
+    _inherit = ['product.product']
+
+    contact_id = fields.Many2one(
+        comodel_name='res.partner',  # tabla a la que se relaciona
+        string="Contacto"  # nombre del campo
+    )
+
+
+class Box(models.Model):
+    _name = 'box.irh'
+    _description = 'Cajas de la empresa'
+
+    name = fields.Selection(
+        string="Nombre",
+        selection=[
+            ('standard', 'Standard'),
+            ('marcas', 'Marcas'),
+            ('decorada', 'Decorada')
+        ],
+        default='Standard'
+
+    )
+    type_box = fields.Selection(
+        string="Tipo de caja",
+        selection=[
+            ('fina', 'Capa fina'),
+            ('normal', 'Capa normal'),
+            ('doble', 'Capa doble')
+        ],
+        default='Capa normal'
+    )
+    supplier = fields.Many2one(
+        comodel_name='res.partner',
+        string="Proveedor"
+    )
+    product = fields.Many2one(
+        comodel_name='product.product',
+        string="Producto"
+    )
